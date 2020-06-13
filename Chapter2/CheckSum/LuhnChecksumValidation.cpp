1afcbcab74f5bb7207e834c9c4cfa67a2b69fb09
mikeg1223 <michael.grossman066@qmail.cuny.edu>
2020-06-13 00:23:50 -0400

    finished chapter two in text work, not onto exercises

--- Chapter2/CheckSum/LuhnChecksumValidation.cpp
+++ Chapter2/CheckSum/LuhnChecksumValidation.cpp
@@ -6,27 +6,52 @@ using std::cout;
 int main()
 {
 	char hold;
+	int evenChecksum = 0;
+	int oddChecksum = 0;
 	int checksum = 0;
-	cout << "Enter a 6 digit number: ";
-	for(int i = 0; i < 6; ++i)
+	cout << "Enter a number number: ";
+	int i = 0;
+	hold = cin.get();
+	while(hold != 10)
 	{
-		cin >> hold;
-		if(i%2 && hold >= '5')
+		if(!(i%2) && hold >= '5')
 		{
-			checksum += (1 + ((hold - '0')*2)%10)
+			evenChecksum += (1 + ((hold - '0')*2)%10);
+			oddChecksum += hold - '0';
 		}
-		else if(i%2)
+		else if(!(i%2))
 		{
-			checksum += ((hold-'0')*2);
+			evenChecksum += ((hold-'0')*2);
+			oddChecksum += hold - '0';
+		}
+		else if (hold >= '5')
+		{
+			evenChecksum += hold - '0';
+			oddChecksum += (1 + ((hold - '0')*2)%10);
 		}
 		else
 		{
-			checksum += hold - '0';
+			evenChecksum += hold - '0';
+			oddChecksum += ((hold - '0')*2);
 		}
+		hold = cin.get();
+	  	++i;
+	}
+	if(!(i%2))
+	{
+		checksum = evenChecksum;
+	}
+	else
+	{
+		checksum = oddChecksum;
 	}
 	if(!(checksum%10))
 	{
-		
+		cout << "This is a valid checksum";
+	}
+	else
+	{
+		cout << "This is an invalid checksum";
 	}
 	return 0;
 }
---
+++ Chapter2/Decode/decode.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	char outputCharacter;
+	enum modeType {UPPERCASE, LOWERCASE, PUNCTUATION};
+	modeType mode = UPPERCASE;
+	char digitChar;
+	do
+	{
+		digitChar = cin.get();
+		int number = (digitChar - '0');
+		digitChar = cin.get();
+		while (digitChar != 10 && digitChar != ',')
+		{
+			number = number *10 + digitChar - '0';
+			digitChar = cin.get();
+		}
+		switch(mode)
+		{
+			case UPPERCASE: 
+				{
+					number %= 27;
+					outputCharacter = number + 'A' - 1;
+					if(!number)
+					{
+						mode = LOWERCASE;
+						continue;
+					}
+					break;
+				}
+			case LOWERCASE:
+				{
+					number %= 27;
+					outputCharacter = number + 'a' -1;
+					if(!number)
+					{
+						mode = PUNCTUATION;
+						continue;
+					}
+					break;
+				}
+			case PUNCTUATION:
+				{
+					number %= 9;
+					switch(number)
+					{
+						case 1: {outputCharacter = '!'; break;}
+						case 2: {outputCharacter = '?'; break;}
+						case 3: {outputCharacter = ','; break;}
+						case 4: {outputCharacter = '.'; break;}
+						case 5: {outputCharacter = ' '; break;}
+						case 6: {outputCharacter = ';'; break;}
+						case 7: {outputCharacter = '"'; break;}
+						case 8: {outputCharacter = '\''; break;}
+					}
+					if (number = 0)
+					{
+						mode = UPPERCASE;
+						continue;
+					}
+					break;
+				}
+		}
+		cout << outputCharacter;
+	} while (digitChar != 10);
+	cout << "\n";
+	return 0;
+}
